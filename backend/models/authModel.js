d8c878fbad9745b901adb8925b62f0313ccff44e
Atharva Nimbalkar <g.brain2753@gmail.com>
2024-06-02 01:20:56 +0530

    env and authModule file modified

--- backend/models/authModel.js
+++ backend/models/authModel.js
@@ -1,5 +1,8 @@
 //database
 
+/* This line of code is importing the `model` and `Schema` objects from the 'mongoose' library in
+Node.js. The `model` object is used to create and access collections in the MongoDB database, while
+the `Schema` object is used to define the structure of the data that will be stored in the database. */
 const {model,Schema}=require('mongoose');
 
 const registerSchema=new Schema({
@@ -24,7 +27,15 @@ const registerSchema=new Schema({
         required:true,
     }
     },
+/* The `{timestamps:true}` option in the Mongoose schema definition automatically adds two fields to
+the documents in the collection: `createdAt` and `updatedAt`. */
     {timestamps:true}
 );    
 
+/* `module.exports=model('user',registerSchema);` is exporting a model named 'user' that is created
+using the registerSchema defined earlier. This model will be used to interact with the 'user'
+collection in the MongoDB database. The `model` function takes two arguments - the name of the
+collection ('user' in this case) and the schema (registerSchema) that defines the structure of the
+documents in that collection. By exporting this model, other parts of the application can import and
+use it to perform CRUD operations on the 'user' collection. */
 module.exports=model('user',registerSchema);
